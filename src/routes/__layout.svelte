c62a0afc343ddf5d30e0c7bd0ca728b3a53f51ab
im-aeo <109118450+im-aeo@users.noreply.github.com>
2022-11-05 01:42:51 -0400

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,66 @@
 <script>
     import "../app.css"
 </script>
-<div class="flex items-center justify-between p-4">
-  <div class="flex items-center gap-2">
-    <div class="mr-5 text-xl">ANOMIA</div>
-    <a href="/"> <div class="">Homepage</div> </a>
-    <a href="/forums"> <div class="">Forums</div> </a>
-    <a href="/sandboxes"> <div class="">Sandboxes</div> </a>
-    <a href="https://discord.gg/JVpFdV8Knp" target="_blank"> <div class="">Discord Server</div> </a>
-    <a href="https://vixlatio.com" target="_blank"> <div class="underline underline-offset-4">Vixlatio</div> </a>
+
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Homepage</a></li>
+        <li tabindex="0">
+          <a class="justify-between">
+            Parent
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+          </a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a class="btn btn-ghost normal-case text-xl">ANOMIA</a>
   </div>
-  <div class="flex items-center gap-2">
-    <a href="/login"> <div class="">Login</div> </a>
-    <a href="/register"> <div class="">Register</div> </a>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal p-0">
+      <li><a>Homepage</a></li>
+      <li tabindex="0">
+        <a>
+          Parent
+          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul class="p-2">
+          <li><a>Submenu 1</a></li>
+          <li><a>Submenu 2</a></li>
+        </ul>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a class="btn">Get started</a>
   </div>
 </div>
 
-<slot/>
+<div class="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <!-- Page content here -->
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+      <slot/>
+  
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer-2" class="drawer-overlay"></label> 
+    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+      <!-- Sidebar content here -->
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  
+  </div>
+</div>
